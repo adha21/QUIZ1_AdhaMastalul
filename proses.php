6303582e0aebe558717e38d5b38c6799a96781e2
adha21 <adhamastalulala@gmail.com>
2024-11-09 10:54:31 +0700

    form hasil input

--- proses.php
+++ proses.php
@@ -13,3 +13,60 @@ $simpan = "INSERT INTO dosen (nidn, nama, rumpun, tempat, tanggal, email) VALUES
 #4. menjalankan query
 mysqli_query($koneksi, $simpan);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Dosen</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-6 m-auto">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Form Dosen</h3>
+                    </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                    <td scope="row">nidn</td>
+                                    <th>: <?=$nidn?></th>
+                                    </tr>
+                                    <tr>
+                                    <td scope="row">Nama Lengkap</td>
+                                    <th>: <?=$nama?></th>
+                                    </tr>
+                                    <tr>
+                                    <td scope="row">Rumpun</td>
+                                    <th>: <?=$rumpun?></th>
+                                    </tr>
+                                    <tr>
+                                    <td scope="row">Tempat Lahir</td>
+                                    <th>: <?=$tempat?></th>
+                                    </tr>
+                                    <tr>
+                                    <td scope="row">Tanggal Lahir</td>
+                                    <th>: <?=$tanggal?></th>
+                                    </tr>
+                                    <tr>
+                                    <td scope="row">Email</td>
+                                    <th>: <?=$email?></th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <a href="index.php" class="btn btn-primary">Kembali</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
